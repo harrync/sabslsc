d0c39ad8ebabadb67b0ccd12c12c3ff1b1540d7c
Harry Edwards <harryedwards.nc@gmail.com>
2022-08-21 19:11:21 +0100

    test build

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
@@ -11,13 +11,10 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="St Austell Bay Surf Lifesaving Club 🏄‍♀️" />
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
